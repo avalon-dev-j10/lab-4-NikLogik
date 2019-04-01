c8fc806e5e6621ff86c17b64f99b6e342933fa1c
Dmitry Kamenschikov <kamenschikov@list.ru>
2019-04-01 03:56:01 +0300

    ЛР№4 Принята!   Отметка  - "Отлично"!

--- src/ru/avalon/java/dev/j10/labs/Person/PersonConst.java
+++ src/ru/avalon/java/dev/j10/labs/Person/PersonConst.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ru.avalon.java.dev.j10.labs.Person;
 
-/**
- *
- * @author java
- */
 enum PersonConst {
     Nikita("Nikita"), Jhon("Jhon"), Thomas("Thomas"), Anton("Anton"), Ellis("Ellis"), NoName;
     
